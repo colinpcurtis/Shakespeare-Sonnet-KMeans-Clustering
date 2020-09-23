affcc450878c6a637209388594aea6365a3d3d27
Colin Curtis <69370385+colinpcurtis@users.noreply.github.com>
2020-09-23 13:14:29 -0500

    Update and rename create_embedding.py to create_integer_embedding.py

--- create_integer_embedding.py
+++ create_integer_embedding.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from poetry_analysis import poems
+from reading_poem_txt_files.py import poems
 from create_word_dict import remove_punctuation
 
 data = pd.read_csv("word_dictionary.csv", header=None)
